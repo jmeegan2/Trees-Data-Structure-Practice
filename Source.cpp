fffa15d3de41e772c2e5a590bea0d81752d35493
jmeegan2 <jmeegan2@owlmail.harford.edu>
2021-05-26 23:44:30 -0400

    format changes

--- Source.cpp
+++ Source.cpp
@@ -57,6 +57,7 @@ int main()
 	
 	return 0;
 }
+
 /*
 Tree is a hierarchical data structure. Main uses of trees include maintaining hierarchical data, 
 providing moderate access and insert/delete operations. Binary trees are special cases of tree
